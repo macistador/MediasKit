3b272710e7e2d409377348e211beb1039492e4a0
Lucas Abijmil <lucas.abijmil@gmail.com>
2024-10-16 20:05:40 +0200

    Wip

--- Sources/MediasKit/CacheService.swift
+++ Sources/MediasKit/CacheService.swift
@@ -20,7 +20,7 @@ protocol CacheServiceLogic: Actor {
     func insert(_ value: Value, forKey key: Key)
     func value(forKey key: Key) -> Value?
     func remove(forKey key: Key)
-    func clear()
+    func removeAll()
 }
 
 final class CacheKey<T: Hashable>: NSObject {
@@ -49,29 +49,69 @@ final class CacheEntry<Value> {
     }
 }
 
+extension URL {
+
+    @available(iOS, obsoleted: 16.0, message: "Use the URL.cachesDirectory built-in property")
+    static var cachesDirectory: URL {
+        if #available(iOS 16.0, *) {
+            return URL.cachesDirectory
+        } else {
+            return FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!
+        }
+    }
+}
+
 actor CacheService<Key: Hashable, Value>: CacheServiceLogic {
-    private let cache = NSCache<CacheKey<Key>, CacheEntry<Value>>()
+    private let memory = NSCache<CacheKey<Key>, CacheEntry<Value>>()
+    private let disk = URL.cachesDirectory
     private let creationDate: Date = .now
     private let expirationTime: TimeInterval
     private let type: CacheType
+    private var isMemoryType: Bool {
+        return type == .memory || type == .memoryAndDisk
+    }
+    private var isDiskType: Bool {
+        return type == .disk || type == .memoryAndDisk
+    }
 
-    init(countLimit: Int = 0, costLimit: Int = 0, expirationTime: TimeInterval = .oneDay, cacheType: CacheType = .memoryAndDisk) {
-        cache.countLimit = countLimit
-        cache.totalCostLimit = costLimit
+    init(countLimit: Int = 0, costLimit: Int = 0, expirationTime: TimeInterval = .oneDay, type: CacheType = .memoryAndDisk) {
+        memory.countLimit = countLimit
+        memory.totalCostLimit = costLimit
         self.expirationTime = expirationTime
-        self.type = cacheType
+        self.type = type
+    }
+
+    func insert(_ value: Value, forKey key: Key) {
+        insertInMemory(value, forKey: key)
+        insertInDisk(value, forKey: key)
+    }
+
+    func value(forKey key: Key) -> Value? {
+////        return valueInCache(forKey: key) ?? valueInDisk(forKey: key)
+////        if let valueInCache = valueInCache(forKey: <#T##Hashable#>)
+//        return nil
     }
 
     // TODO: memory
-    func insert(_ value: Value, forKey key: Key) {
+    func remove(forKey key: Key) {
+
+    }
+
+    // TODO: memory
+    func removeAll() {
+        removeAllInMemory()
+    }
+
+    // MARK: - Memory
+    private func insertInMemory(_ value: Value, forKey key: Key) {
+        guard isMemoryType else { return }
         let expirationDate = creationDate.addingTimeInterval(expirationTime)
         let entry = CacheEntry(value: value, expirationDate: expirationDate)
-        cache.setObject(entry, forKey: CacheKey(key))
+        memory.setObject(entry, forKey: CacheKey(key))
     }
 
-    // TODO: memory
-    func value(forKey key: Key) -> Value? {
-        guard let cacheEntry = cache.object(forKey: CacheKey(key)) else { return nil }
+    private func valueInMemory(forKey key: Key) -> Value? {
+        guard isMemoryType, let cacheEntry = memory.object(forKey: CacheKey(key)) else { return nil }
         guard cacheEntry.expirationDate > creationDate else {
             remove(forKey: key)
             return nil
@@ -79,13 +119,31 @@ actor CacheService<Key: Hashable, Value>: CacheServiceLogic {
         return cacheEntry.value
     }
 
-    // TODO: memory
-    func remove(forKey key: Key) {
-        cache.removeObject(forKey: CacheKey(key))
+    private func removeInMemory(forKey key: Key) {
+        guard isMemoryType else { return }
+        memory.removeObject(forKey: CacheKey(key))
     }
 
-    // TODO: memory
-    func clear() {
-        cache.removeAllObjects()
+    private func removeAllInMemory() {
+        guard isMemoryType else { return }
+        memory.removeAllObjects()
+    }
+
+    // MARK: - Disk
+    private func insertInDisk(_ value: Value, forKey key: Key) {
+        guard isDiskType else { return }
+    }
+
+    private func valueInDisk(forKey key: Key) -> Value? {
+        guard isDiskType else { return nil }
+        return nil
+    }
+
+    private func removeInDisk(forKey key: Key) {
+        guard isDiskType else { return }
+    }
+
+    private func removeAllInDisk() {
+        guard isDiskType else { return }
     }
 }
